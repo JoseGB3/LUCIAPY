a9e453b5132e52b8f4bb49a39115a1e5a10b95b3
JoseGB3 <jose.gb3@puccampinas.edu.br>
2026-03-02 17:19:57 -0300

    Update ex2lista2.py

--- ex2lista2.py
+++ ex2lista2.py
@@ -1,4 +1,5 @@
 
 f=float(input("Digite a temperatura em graus fahrenheit: "))
 c=(f-32)*5/9
-print(f"O valor convertido de fahrenheit pra graus celcius é: {c:.2f},°C")
+print(f"O valor convertido de fahrenheit pra graus celcius é: {c:.2f} °C")
+
