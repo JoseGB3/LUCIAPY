7a7d3fa5de25b44e7ae8c43d9eeaa8989ee585db
JoseGB3 <jose.gb3@puccampinas.edu.br>
2026-03-09 09:03:29 -0300

    desconto if<100

---
+++ ex6l3.py
@@ -0,0 +1,6 @@
+valor=float(input("Digite o valor do produto:R$"))
+if(valor>
+   100):
+    print("O valor do produto após o desconto é:R$",valor*0.9)
+else:
+    print("O produto não possui desconto.")
