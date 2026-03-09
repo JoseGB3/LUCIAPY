8d2a0817d9d848c684946b0caf411b3b0e9cce9c
JoseGB3 <jose.gb3@puccampinas.edu.br>
2026-03-09 08:59:16 -0300

    ex5l3

---
+++ numerosdivisiveis.py
@@ -0,0 +1,7 @@
+
+n1=int(input("Digite um valor númerico: "))
+n2=int(input("Digite outro valor númerico: "))
+if(n1%n2==0):
+    print("Os números são divisiveis, e o resultado da divisão é:",n1/n2)
+else:
+              print("Os numeros não são divisiveis.")
